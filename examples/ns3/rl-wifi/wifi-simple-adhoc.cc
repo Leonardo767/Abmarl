712e4fff62871e1982c0c76936aef4c8f628a94a
Hennis <hennis1@luz.llnl.gov>
2020-12-22 18:18:38 -0800

    Created the capability to have multiple nodes in ns3.

--- examples/ns3/rl-wifi/wifi-simple-adhoc.cc
+++ examples/ns3/rl-wifi/wifi-simple-adhoc.cc
@@ -49,7 +49,8 @@
 //
 // tcpdump -r wifi-simple-adhoc-0-0.pcap -nn -tt
 //
-
+#include <limits>
+#include <random>
 #include "ns3/command-line.h"
 #include "ns3/config.h"
 #include "ns3/double.h"
@@ -61,7 +62,7 @@
 #include "ns3/yans-wifi-channel.h"
 #include "ns3/mobility-model.h"
 #include "ns3/internet-stack-helper.h"
-
+#include "ns3/flow-monitor-module.h"
 #include "ns3/ns3-ai-module.h"
 
 using namespace ns3;
@@ -103,27 +104,33 @@ Ns3RlEnv::Ns3RlEnv(uint16_t id) : Ns3AIRL<NS3Environment, NS3AgentActions>(id)
 
 Ns3RlEnv * my_env = new Ns3RlEnv(1234);
 
+static int packetsReceived = 0;
+
 void ReceivePacket (Ptr<Socket> socket)
 {
-  static int packetsReceived = 0;
 
   while (socket->Recv ())
-    {
-      NS_LOG_UNCOND ("Received one packet! " << ++packetsReceived);
-      auto env = my_env->EnvSetterCond();
-      env->socketUid = 2;
-      env->envType = 1;
-      env->simTime_us = Simulator::Now().GetMicroSeconds();
-      env->nodeId = 5;
-      env->segmentSize = 200;
-      NS_LOG_UNCOND ("Time: " << (uint64_t)(Simulator::Now().GetMilliSeconds()) << " env->ssThresh: " << env->ssThresh << " env->cWnd: "
-                << env->cWnd << " env->segmentSize: " << env->segmentSize);
-      my_env->SetCompleted();
-    }
-  NS_LOG_UNCOND("ReceivePacket: function exited.");
+  {
+    ++packetsReceived;
+//    NS_LOG_UNCOND ("Received one packet! " << packetsReceived);
+  }
+//  NS_LOG_UNCOND("ReceivePacket: function exited. " << "Time: " << (uint64_t)(Simulator::Now().GetNanoSeconds()) << " ID: " << socket->GetNode()->GetId());
 }
 
-static void GenerateTraffic (Ptr<Socket> socket, uint32_t pktSize,
+static void SendReceivedPacketData(Ptr<Socket> socket, uint32_t pktSize,
+                                   uint32_t pktCount, Time pktInterval) {
+  auto env = my_env->EnvSetterCond();
+  env->socketUid = 2;
+  env->envType = 1;
+  env->simTime_us = Simulator::Now().GetMicroSeconds();
+  env->nodeId = 5;
+  env->segmentSize = packetsReceived;
+  NS_LOG_UNCOND ("Time: " << (uint64_t)(Simulator::Now().GetNanoSeconds()) << " env->ssThresh: " << env->ssThresh << " env->cWnd: "
+                 << env->cWnd << " Packets Received: (env->segmentSize): " << env->segmentSize);
+  my_env->SetCompleted();
+}
+
+static void GenerateTraffic (std::vector<Ptr<Socket>> & sockets, uint32_t pktSize,
                              uint32_t pktCount, Time pktInterval )
 {
   auto act = my_env->ActionGetterCond();
@@ -131,32 +138,67 @@ static void GenerateTraffic (Ptr<Socket> socket, uint32_t pktSize,
   static bool endSimulation = false;
   static int numPackets = 0;
 
-  if(firstTime == true){
-    firstTime = false;
-    socket->Send (Create<Packet> (pktSize));
-    ++numPackets;
-  }
-
   if(act->new_ssThresh == 1){
-    NS_LOG_UNCOND ("Time: " << Simulator::Now().GetMilliSeconds() << " new_cWnd: " << act->new_cWnd << " new_ssThresh: " << act->new_ssThresh);
+    NS_LOG_UNCOND ("Time: " << Simulator::Now().GetNanoSeconds() << " new_cWnd: " << act->new_cWnd << " ID: " << sockets[0]->GetNode()->GetId());
     act->new_ssThresh = 0;
-    ++numPackets;
-    NS_LOG_UNCOND("GenerateTraffic: Sending new packet. PktCount: " << numPackets);
-    socket->Send (Create<Packet> (pktSize));
+    for (int index = 0; index < pktCount; ++index) {
+      ++numPackets;
+      NS_LOG_UNCOND("GenerateTraffic: Sending new packet. PktCount: " << numPackets << " Time: " << (uint64_t)(Simulator::Now().GetNanoSeconds()));
+      sockets[0]->Send(Create<Packet> (pktSize));
+    }
   } else if (act->new_ssThresh == 1234) {
     NS_LOG_UNCOND("GenerateTraffic: Closing socket.");
-    socket->Close ();
+    sockets[0]->Close ();
     my_env->SetFinish();
     endSimulation = true;
     NS_LOG_UNCOND("GenerateTraffic: Socket closed, environment finished.");
   } else {
-    NS_LOG_UNCOND("ERROR, incorrect state.");
+    if(firstTime == true){
+      firstTime = false;
+      sockets[0]->Send (Create<Packet> (pktSize));
+      ++numPackets;
+    } else {
+      NS_LOG_UNCOND("ERROR, incorrect state.");
+    }
   }
   if(endSimulation == false){
+    const double smallestTimeUnit = 0.001; //millisecond
+    Time smallTimeInterval = Seconds (smallestTimeUnit);
+    Time receiveInterval = pktInterval - smallTimeInterval;
+    //schedule the received packet callback to send pkt data to agents
+    Simulator::Schedule (receiveInterval, &SendReceivedPacketData,
+                         sockets[0], pktSize,pktCount, pktInterval);
     Simulator::Schedule (pktInterval, &GenerateTraffic,
-                         socket, pktSize,pktCount, pktInterval);
+                         sockets, pktSize,pktCount, pktInterval);
+  } else {
+    Simulator::Stop();
   }
 }
+
+uint32_t numNodes = 4;
+
+static void GenerateTraffic2 (std::vector<Ptr<Socket>> & sockets, uint32_t pktSize,
+                             uint32_t pktCount, Time pktInterval)
+{
+  static int numPackets = 0;
+  static int nodeNum = 0;
+
+  for (int index = 0; index < pktCount; ++index) {
+      ++numPackets;
+//      NS_LOG_UNCOND("GenerateTraffic2: Sending new packet. PktCount: " << numPackets
+//                    << " Time: " << (uint64_t)(Simulator::Now().GetNanoSeconds())
+//                    << " ID: " << sockets[nodeNum]->GetNode()->GetId());
+      sockets[nodeNum]->Send (Create<Packet> (pktSize));
+  }
+  if(nodeNum < numNodes-1){
+    ++nodeNum;
+  } else {
+    nodeNum = 0;
+  }
+  Simulator::Schedule (pktInterval, &GenerateTraffic2,
+                       sockets, pktSize,pktCount, pktInterval);
+}
+
 
 int main (int argc, char *argv[])
 {
@@ -166,13 +208,16 @@ int main (int argc, char *argv[])
   uint32_t numPackets = 1;
   double interval = 1.0; // seconds
   bool verbose = false;
+  double distMultiplier = 10.0;
 
   CommandLine cmd;
   cmd.AddValue ("phyMode", "Wifi Phy mode", phyMode);
   cmd.AddValue ("rss", "received signal strength", rss);
   cmd.AddValue ("packetSize", "size of application packet sent", packetSize);
-  cmd.AddValue ("numPackets", "number of packets generated", numPackets);
-  cmd.AddValue ("interval", "interval (seconds) between packets", interval);
+  cmd.AddValue ("numPackets", "number of packets generated per interval", numPackets);
+  cmd.AddValue ("interval", "interval (seconds) between RL steps", interval);
+  cmd.AddValue ("numNodes", "the number of nodes in this scenario", numNodes);
+  cmd.AddValue ("distMultiplier", "the multiplier for distance between nodes, actual distance will be random", distMultiplier);
   cmd.AddValue ("verbose", "turn on all WifiNetDevice log components", verbose);
   cmd.Parse (argc, argv);
   // Convert to time object
@@ -183,7 +228,7 @@ int main (int argc, char *argv[])
                       StringValue (phyMode));
 
   NodeContainer c;
-  c.Create (2);
+  c.Create (numNodes);
 
   // The below set of helpers will help us to put together the wifi NICs we want
   WifiHelper wifi;
@@ -221,8 +266,26 @@ int main (int argc, char *argv[])
   // used for received signal strength.
   MobilityHelper mobility;
   Ptr<ListPositionAllocator> positionAlloc = CreateObject<ListPositionAllocator> ();
-  positionAlloc->Add (Vector (0.0, 0.0, 0.0));
-  positionAlloc->Add (Vector (5.0, 0.0, 0.0));
+  for(int index = 0; index < numNodes; ++index){
+    std::random_device rd1;
+    std::mt19937 gen1(rd1());
+    std::uniform_real_distribution<> distribution1(0, 1);
+    double random1 = distribution1(gen1) * distMultiplier;
+    std::random_device rd2;
+    std::mt19937 gen2(rd2());
+    std::uniform_real_distribution<> distribution2(0, 1);
+    double random2 = distribution2(gen2) * distMultiplier;
+    std::random_device rd3;
+    std::mt19937 gen3(rd3());
+    std::uniform_real_distribution<> distribution3(0, 1);
+    double random3 = distribution3(gen3) * distMultiplier;
+    positionAlloc->Add (Vector (random1, random2, random3));
+
+    std::cout.precision(std::numeric_limits<double>::max_digits10);
+    std::cout << "Random number1: " << random1
+              << " Random number2: " << random2
+              << " Random number3: " << random3 << std::endl;
+  }
   mobility.SetPositionAllocator (positionAlloc);
   mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
   mobility.Install (c);
@@ -236,15 +299,21 @@ int main (int argc, char *argv[])
   Ipv4InterfaceContainer i = ipv4.Assign (devices);
 
   TypeId tid = TypeId::LookupByName ("ns3::UdpSocketFactory");
-  Ptr<Socket> recvSink = Socket::CreateSocket (c.Get (0), tid);
-  InetSocketAddress local = InetSocketAddress (Ipv4Address::GetAny (), 80);
-  recvSink->Bind (local);
-  recvSink->SetRecvCallback (MakeCallback (&ReceivePacket));
+  for(int index = 0; index < numNodes; ++index){
+    Ptr<Socket> recvSink = Socket::CreateSocket (c.Get (index), tid);
+    InetSocketAddress local = InetSocketAddress (Ipv4Address::GetAny (), 80);
+    recvSink->Bind (local);
+    recvSink->SetRecvCallback (MakeCallback (&ReceivePacket));
+  }
 
-  Ptr<Socket> source = Socket::CreateSocket (c.Get (1), tid);
-  InetSocketAddress remote = InetSocketAddress (Ipv4Address ("255.255.255.255"), 80);
-  source->SetAllowBroadcast (true);
-  source->Connect (remote);
+  std::vector<Ptr<Socket>> sources;
+  for(int index = 0; index < numNodes; ++index){
+    Ptr<Socket> source = Socket::CreateSocket (c.Get (index), tid);
+    InetSocketAddress remote = InetSocketAddress (Ipv4Address ("255.255.255.255"), 80);
+    source->SetAllowBroadcast (true);
+    source->Connect (remote);
+    sources.push_back(source);
+  }
 
   // Tracing
   wifiPhy.EnablePcap ("wifi-simple-adhoc", devices);
@@ -253,9 +322,21 @@ int main (int argc, char *argv[])
   NS_LOG_UNCOND ("Testing " << numPackets  << " packets sent with receiver rss " << rss );
   auto env = Create<Ns3RlEnv> (1234);
   NS_LOG_UNCOND ("CreateEnv: " << (env == 0));
-  Simulator::ScheduleWithContext (source->GetNode ()->GetId (),
+
+  Simulator::ScheduleWithContext (sources[0]->GetNode ()->GetId (),
                                   Seconds (1.0), &GenerateTraffic,
-                                  source, packetSize, numPackets, interPacketInterval);
+                                  sources, packetSize, numPackets, interPacketInterval);
+
+  for(int index = 0; index < numNodes; ++index){
+    std::random_device rd1;
+    std::mt19937 gen1(rd1());
+    std::uniform_real_distribution<> distribution1(0, 1);
+    double random1 = distribution1(gen1);
+    Simulator::ScheduleWithContext (sources[index]->GetNode ()->GetId (),
+                                    Seconds(1.0) + Seconds(random1), &GenerateTraffic2,
+                                    sources, packetSize, numPackets, interPacketInterval);
+  }
+
   Simulator::Run ();
   Simulator::Destroy ();
 
